1f9a27488996185c725c390a82ce85cbf4abcd0a
Swetha Suresh <swethasuresh@canvendor.com>
2026-01-28 20:17:12 +0530

    adding jenkins file

--- nppy-master/app.py
+++ nppy-master/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello from Python CI/CD App"})
+    return "Hello from Docker + Jenkins + Nexus"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
